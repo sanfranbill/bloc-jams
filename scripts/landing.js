64a458c0086479dc7a4899805fa2c1775b96ae3a
Bill Tang <laptopbatteryusa@gmail.com>
2017-03-16 17:38:21 -0700

    Corrected checkpoint 8

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,13 +2,13 @@ var animatePoints = function(){
     var points = document.getElementsByClassName('point');
  
     var revealPoint = function(index) {
-        points[index].style.opacity = .5;
+        points[index].style.opacity = 0.5;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
  
-    for (var i=0; i < points.length; index++) {
+    for (var i=0; i < points.length; i++) {
         revealPoint(i);
     }
 };

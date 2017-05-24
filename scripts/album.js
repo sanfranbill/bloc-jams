08728b1e7b3f1cb64466ca9f9cd4c2cd7b5bf513
sanfranbill <laptopbatteryusa@gmail.com>
2017-05-24 09:18:41 -0700

    finished assignment

--- scripts/album.js
+++ scripts/album.js
@@ -81,8 +81,12 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null){
-            currentParent = currentParent.parentElement;
+        if (currentParent.className == null) {
+            console.log("No parent found");
+        } else if (currentParent.className !== targetClass) {
+            console.log("No parent found with that class name");
+        } else {
+            currentParent = currentParent.parentElement; 
         }
         return currentParent;
     }

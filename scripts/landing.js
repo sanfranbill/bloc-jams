c6f2ed0f85d96486422ea3c6b14227daf399f170
sanfranbill <laptopbatteryusa@gmail.com>
2017-06-14 12:29:15 -0700

    finished checkpoint 16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,28 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points){
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+var animatePoints = function() {
+    var revealPoint = function() {
+        // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scale(1) translateY(0)'
+        });
     
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    };
+    // #6
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
+$(window).load(function() {
     // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+    if ($(window).height() > 950) {
+         animatePoints();
     }
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    
     // this will trigger the animation when the user scrolls at least 200 pixels into the 'selling points' element
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
     
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();   
         }
     });
-}
+});
